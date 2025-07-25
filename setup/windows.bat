74b4d952cbfc85e96e483bdee4f7fcf440d693f8
kittycathy233 <2286431462@qq.com>
2025-07-25 13:53:03 +0800

    我不想优化Android端，因为我不会

--- setup/windows.bat
+++ setup/windows.bat
@@ -4,7 +4,7 @@ cd ..
 echo Installing dependencies...
 echo This might take a few moments depending on your internet speed.
 haxelib git hxcpp https://github.com/kittycathy233/hxcpp --quiet
-haxelib install lime 8.1.3 --quiet
+haxelib git lime https://github.com/kittycathy233/lime --quiet
 haxelib install openfl 9.4.1 --quiet
 haxelib install flixel 5.9.0 --quiet
 haxelib install flixel-addons 3.3.2 --quiet
